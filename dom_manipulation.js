17c025672049885364777ac25bc68f4e7ff24e53
baori <john.guibao127@gmail.com>
2023-01-26 17:01:30 +0000

    Javascript: Added elemetn to a node

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -9,3 +9,17 @@ div.textContent = "Hey I'm Red!"; //text within div element
 div.setAttribute('style', 'color: red');//attributes of text
 
 container.appendChild(div); //adding div to container node
+
+const div2 = document.createElement('div');
+div2.innerHTML = "<h3>I'm a blue h3<h3>";
+div2.firstChild.setAttribute('style', 'color: blue');
+
+container.appendChild(div2);
+
+/*an <h3> with blue text that says “I’m a blue h3!”
+a <div> with a black border and pink background color with the following elements inside of it:
+
+another <h1> that says “I’m in a div”
+a <p> that says “ME TOO!”
+Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
+*/

2ffe137a2086fd9728c4cca233961c12e7503e4a
baori <john.guibao127@gmail.com>
2023-01-26 17:11:01 +0000

    Javascript: Added Multiple Elements to Node

--- dom_manipulation.js
+++ dom_manipulation.js
@@ -1,7 +1,4 @@
 //<p> with red text
-const p = document.createElement('p'); 
-
-
 const container = document.querySelector('#container'); //selects first element with container id 
 
 const div = document.createElement('div')//creates div element
@@ -10,16 +7,28 @@ div.setAttribute('style', 'color: red');//attributes of text
 
 container.appendChild(div); //adding div to container node
 
+//an <h3> with blue text that says “I’m a blue h3!”
 const div2 = document.createElement('div');
 div2.innerHTML = "<h3>I'm a blue h3<h3>";
 div2.firstChild.setAttribute('style', 'color: blue');
 
 container.appendChild(div2);
 
-/*an <h3> with blue text that says “I’m a blue h3!”
-a <div> with a black border and pink background color with the following elements inside of it:
+//a <div> with a black border and pink background color
+const div3 = document.createElement('div');
+div3.setAttribute('style', 'border: 1px solid black; background-color: pink'); 
 
-another <h1> that says “I’m in a div”
-a <p> that says “ME TOO!”
-Hint for this one: after creating the <div> with createElement, append the <h1> and <p> to it before adding it to the container.
-*/
+//<h1> that says “I’m in a div”
+const h1 = document.createElement('h1');
+h1.innerText = "I'm in a div";
+
+//<p> that says “ME TOO!”
+const p = document.createElement('p'); 
+p.innerText = "ME TOO!";
+
+//adding elements too div3 
+div3.appendChild(h1);
+div3.appendChild(p);
+
+container.appendChild(div3);
+
